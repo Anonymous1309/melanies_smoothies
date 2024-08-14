a13ede85131d26c571d583081c858a1e64565229
Anonymous1309 <chelagamsetty.ishitha@cdw.com>
2024-08-14 14:39:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,6 @@ name_on_order=st.text_input("name on smoothie")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'),col('search_on'))
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
 st.dataframe(data=my_dataframe ,use_container_width=True)
 ingredients_list=st.multiselect('Choose maximum of 5 fruits',my_dataframe,max_selections=5)
 if ingredients_list:
@@ -28,9 +27,7 @@ if ingredients_list:
         st.subheader( each_fruit+ 'Nutrient_Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+each_fruit)
         fv_dataframe = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
-    st.write(ingredients_string);
     sql_insert="""insert into smoothies.public.orders(ingredients,name_on_order) values('"""+ingredients_string+"""','"""+name_on_order+"""')""";
-    st.write(sql_insert);
     submit_button=st.button('submit order');
     if submit_button:
         session.sql(sql_insert).collect()
