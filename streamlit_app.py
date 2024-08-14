a1ce8ca6f3b510fb29f75169e5edce297a1808fd
Anonymous1309 <chelagamsetty.ishitha@cdw.com>
2024-08-14 14:40:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,6 @@ name_on_order=st.text_input("name on smoothie")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'),col('search_on'))
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.dataframe(data=my_dataframe ,use_container_width=True)
 ingredients_list=st.multiselect('Choose maximum of 5 fruits',my_dataframe,max_selections=5)
 if ingredients_list:
     ingredients_string='';
